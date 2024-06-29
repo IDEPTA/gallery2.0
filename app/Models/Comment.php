a22988dbf4e538594615ddac3ad7fd69efefcd5e
IDEPTA <procopchuk14092002@yandex.ru>
2024-06-29 09:50:53 +0300

    add main models and migrations

---
+++ app/Models/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'text',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
